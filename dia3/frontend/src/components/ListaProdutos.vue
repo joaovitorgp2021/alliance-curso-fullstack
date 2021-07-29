7e4516f03e303be1b1b638cbb4e7213fc89d6b51
Joao Vitor <joaovitorgp2021@gmail.com>
2021-07-29 08:52:48 -0300

    Att diária

---
+++ dia3/frontend/src/components/ListaProdutos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand>Produtos</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-button>
+          <b-icon-plus></b-icon-plus>
+        </b-button>
+      </b-navbar-nav>
+      <b-modal :id="'test-'+ produto.codigo" title="insert" 
+      ok-variant='danger' cancel-variant="success">
+      
+  <p> Tem certeza que deseja excluir </p>
+      
+    </b-navbar>
+    <ul id="listaProdutos">
+        <li v-for="produto, index in produtos" v-bind:key="produto.codigo">
+            <Produto v-bind:produto="produto" v-on:removed="removerProduto(index)"/>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
---
+++ dia3/frontend/src/components/Produto.vue
@@ -0,0 +1,54 @@
+<template>
+   <div>
+    <b-card id="card-produto" class="text-center"
+      :title="produto.descricao"
+      :sub-title="produto.codigo"
+      >
+      <b-card-text class="bg-secondary text-light">Quantidade {{produto.quantidade}}</b-card-text>
+      <b-button class="float-right" v-b-modal="'modal-confirm-delete-'+produto.codigo">
+        <b-icon-trash></b-icon-trash>
+      </b-button>
+    </b-card>
+    <b-modal :id="'modal-confirm-delete-'+ produto.codigo" title="Confirmação" 
+      ok-variant='danger' cancel-variant="success"
+      ok-title='Sim' cancel-title='Não'
+      v-on:ok="excluir(produto)"
+      >
+      <p>Tem certeza que deseja excluir o produto</p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "Produto",
+  props: {
+    produto: {
+      codigo: Number,
+      descricao: String,
+      quantidade: Number
+    }
+  },
+  methods: {
+    excluir: async function (produto) {
+      // envie o comando de excluir para o backend
+      await axios.delete(`http://localhost:3000/produtos/${produto.codigo}`)
+      // avisar que eu fui excluido
+      this.$emit('removed');
+    }
+  }
+}
+</script>
+<style scoped>
+.card {
+    margin-bottom: 10px;
+    max-width: 320px;
+}
+.card-title {
+  text-align: left;
+}
+.card-subtitle {
+  text-align: left;
+}
+</style>
